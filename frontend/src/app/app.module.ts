663ca154b45c7108f8291403439c96d5ba6d0183
DiogoCesar_victsoft <diogo@victsoft.com.br>
2021-08-11 18:11:48 -0300

    nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NavComponent }  from '@angular/material/sidenav';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent }  from './components/template/nav/nav.component';
 
-
-
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +23,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
